38a0cd8efc87d7c52434e3152d60c42881104bda
Kaj Van der Hallen <kaj.vanderhallen@foresee.be>
2016-01-12 22:40:27 +0100

    03 - Nao wants to talk

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 #!flask/bin/python
 from flask import Flask, abort, jsonify, request
-
+from naoqi import ALProxy
 
 app = Flask(__name__)
 
+nao_host = "localhost"
+nao_port = 53417
 
 @app.route('/')
 def index():
     return "Hello, To The Point!"
+
+@app.route('/greet/<string:name>')
+def greet(name):
+    tts = ALProxy("ALTextToSpeech", nao_host, nao_port)
+    tts.say("Hello "+ str(name) +"!")
+    return "I just greeted "+ str(name) +"!", 200
 
 robots = [
     {
